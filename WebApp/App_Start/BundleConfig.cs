d9dfe132486f36c043d4fc002917632379acbdf7
Mohammad Azami <mohammadazami98@yahoo.com>
2019-08-29 01:44:39 +0430

    Create_Project

---
+++ WebApp/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Optimization;
+
+namespace Atarbashi.WebApp.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                       "~/App_Themes/Atarbashi/vendors/jquery/jquery-{version}.min.js",
+                       "~/App_Themes/Atarbashi/vendors/bootstrap/bootstrap.bundle.min.js",
+                       "~/App_Themes/Atarbashi/vendors/skrollr.min.js",
+                       "~/App_Themes/Atarbashi/vendors/owl-carousel/owl.carousel.min.js",
+                       "~/App_Themes/Atarbashi/vendors/nice-select/jquery.nice-select.min.js",
+                       "~/App_Themes/Atarbashi/vendors/jquery.ajaxchimp.min.js",
+                       "~/App_Themes/Atarbashi/vendors/mail-script.js",
+                       "~/App_Themes/Atarbashi/js/main.js",
+                       "~/App_Themes/Atarbashi/js/atarbashi.js"));
+
+          
+
+            bundles.Add(new StyleBundle("~/bootstrap/css").Include(
+                     "~/App_Themes/Atarbashi/vendors/bootstrap/bootstrap.min.css",
+                     "~/App_Themes/Atarbashi/vendors/fontawesome/css/all.min.css",
+                     "~/App_Themes/Atarbashi/vendors/themify-icons/themify-icons.css",
+                     "~/App_Themes/Atarbashi/vendors/nice-select/nice-select.css",
+                     "~/App_Themes/Atarbashi/vendors/owl-carousel/owl.theme.default.min.css",
+                     "~/App_Themes/Atarbashi/vendors/owl-carousel/owl.carousel.min.css",
+                     "~/App_Themes/Atarbashi/css/style.css"));
+
+        }
+    }
+}
---
+++ WebApp/Areas/ManagementContent/ManagementContentAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Atarbashi.Areas.ManagementContent
+{
+    public class ManagementContentAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ManagementContent";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ManagementContent_default",
+                "ManagementContent/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
