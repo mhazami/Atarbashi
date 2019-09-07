af2f953610ff5b65c707e4007b7af22ca493615a
Mohammad Azami <mohammadazami98@yahoo.com>
2019-09-08 01:08:31 +0430

    13980616

---
+++ BLL/MenuBO.cs
@@ -0,0 +1,15 @@
+﻿using Atarbashi.DataStructure;
+using Azami.Framework.BO;
+using System;
+using System.Collections.Generic;
+
+namespace Atarbashi.BLL
+{
+    public class MenuBO : BaseBO<Menu>
+    {
+        public List<Menu> GetMenuByUserId(Guid userId)
+        {
+            return null;
+        }
+    }
+}
---
+++ BLL/RoleBO.cs
@@ -0,0 +1,9 @@
+﻿using Atarbashi.DataStructure;
+using Azami.Framework.BO;
+
+namespace Atarbashi.BLL
+{
+    public class RoleBO : BaseBO<Role>
+    {
+    }
+}
